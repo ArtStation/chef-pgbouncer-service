56eb6a6cade3f9ba505e8768ed76bf1fb07929a2
Hervé <dud225@users.noreply.github.com>
2020-09-02 20:50:29 +0200

    INFRA-2125: Always restart PgBouncer (#2)

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Konstantin Gredeskoul'
 maintainer_email 'kigster@gmail.com'
 license 'MIT'
 description 'Installs pgBouncer from either sources or packages, configures the connections, and sets up a service.'
-version '0.3.0'
+version '0.3.1'
 chef_version '>= 12'
 
 supports 'ubuntu'
